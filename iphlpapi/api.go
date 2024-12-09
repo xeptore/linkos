e95b971acabdc19650142cf2dc28688710e6df3b
xeptore <29199390+xeptore@users.noreply.github.com>
2024-12-09 23:07:44 +0330

    lint: fix errors
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- iphlpapi/api.go
+++ iphlpapi/api.go
@@ -19,17 +19,17 @@ import (
 
 var (
 	iphlpapi                        = windows.MustLoadDLL("iphlpapi.dll")
-	initializeUnicastIpAddressEntry = iphlpapi.MustFindProc("InitializeUnicastIpAddressEntry")
-	createUnicastIpAddressEntry     = iphlpapi.MustFindProc("CreateUnicastIpAddressEntry")
+	initializeUnicastIPAddressEntry = iphlpapi.MustFindProc("InitializeUnicastIpAddressEntry")
+	createUnicastIPAddressEntry     = iphlpapi.MustFindProc("CreateUnicastIpAddressEntry")
 )
 
-func InitializeUnicastIpAddressEntry() (row C.MIB_UNICASTIPADDRESS_ROW) {
-	_, _, _ = initializeUnicastIpAddressEntry.Call(uintptr(unsafe.Pointer(&row)))
+func InitializeUnicastIPAddressEntry() (row C.MIB_UNICASTIPADDRESS_ROW) {
+	_, _, _ = initializeUnicastIPAddressEntry.Call(uintptr(unsafe.Pointer(&row)))
 	return
 }
 
-func CreateUnicastIpAddressEntry(row *C.MIB_UNICASTIPADDRESS_ROW) error {
-	ret, _, _ := createUnicastIpAddressEntry.Call(uintptr(unsafe.Pointer(row)))
+func CreateUnicastIPAddressEntry(row *C.MIB_UNICASTIPADDRESS_ROW) error {
+	ret, _, _ := createUnicastIPAddressEntry.Call(uintptr(unsafe.Pointer(row)))
 	if err := windows.Errno(ret); !errors.Is(err, windows.ERROR_SUCCESS) {
 		return err
 	}
@@ -37,13 +37,13 @@ func CreateUnicastIpAddressEntry(row *C.MIB_UNICASTIPADDRESS_ROW) error {
 }
 
 func SetAdapterIPv4(luid uint64, ip []byte, subnet int) (err error) {
-	row := InitializeUnicastIpAddressEntry()
+	row := InitializeUnicastIPAddressEntry()
 	ipv4 := (*C.struct_sockaddr_in)(unsafe.Pointer(&row.Address))
 	ipv4.sin_family = C.AF_INET
 	copy(ipv4.sin_addr.S_un[:], ip)
 	row.OnLinkPrefixLength = C.uchar(subnet)
 	row.DadState = C.IpDadStatePreferred
 	*(*uint64)(unsafe.Pointer(&row.InterfaceLuid)) = luid
-	err = CreateUnicastIpAddressEntry(&row)
+	err = CreateUnicastIPAddressEntry(&row)
 	return
 }
