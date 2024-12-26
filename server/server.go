8834d6b5e147e104c0e811888b65c318588c4df7
mbehboodian <29199390+xeptore@users.noreply.github.com>
2024-12-26 14:08:49 -0500

    Revert "Use buffer pool on server"

--- server/server.go
+++ server/server.go
@@ -21,14 +21,12 @@ import (
 	"github.com/xeptore/linkos/config"
 	"github.com/xeptore/linkos/errutil"
 	"github.com/xeptore/linkos/iputil"
-	"github.com/xeptore/linkos/pool"
 )
 
 type (
 	Server struct {
 		gnet.BuiltinEventEngine
 		engine      gnet.Engine
-		pool        pool.Pool
 		bindHost    string
 		bindDev     string
 		bufferSize  int
@@ -83,7 +81,6 @@ func New(logger zerolog.Logger, ipNet, bindHost, bindDev string, bufferSize int)
 	server := &Server{
 		BuiltinEventEngine: gnet.BuiltinEventEngine{},
 		engine:             gnet.Engine{},
-		pool:               pool.New(bufferSize),
 		bindHost:           bindHost,
 		bindDev:            bindDev,
 		bufferSize:         bufferSize,
@@ -187,16 +184,11 @@ func (s *Server) OnTick() (time.Duration, gnet.Action) {
 }
 
 func (s *Server) OnTraffic(conn gnet.Conn) gnet.Action {
-	p := s.pool.AcquirePacket()
-	defer p.ReturnToPool()
-
-	packetBytes := p.Payload.Bytes()
-	n, err := conn.Read(packetBytes)
+	packet, err := conn.Next(-1)
 	if nil != err {
 		s.logger.Error().Err(err).Func(errutil.TreeLog(err)).Msg("Failed to read packet")
 		return gnet.Close
 	}
-	packet := packetBytes[:n]
 
 	localAddr := conn.LocalAddr().String()
 	localAddrPort, err := netip.ParseAddrPort(localAddr)
