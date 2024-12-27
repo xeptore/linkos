78196474d7d18cdb61d309cc1e6507bcbe7c66d7
xeptore <29199390+xeptore@users.noreply.github.com>
2024-12-27 10:28:01 +0330

    config: reduce default kernel snd/rcv buffer sizes
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- config/default.go
+++ config/default.go
@@ -5,17 +5,30 @@ import (
 )
 
 const (
+	second = 1
+	minute = 60 * second
+	hour   = 60 * minute
+)
+
+const (
+	b   = 1
+	kib = 1024 * b
+	mib = 1024 * kib
+	gib = 1024 * mib
+)
+
+const (
 	DefaultServerLogLevel           = zerolog.InfoLevel
 	DefaultClientLogLevel           = zerolog.InfoLevel
 	DefaultTunRingSize              = 1
 	DefaultClientTunDeviceMTU       = 1280
 	DefaultBufferSize               = 1280
 	DefaultServerMaxClients         = 10
-	DefaultServerCleanupIntervalSec = 5 * 60
+	DefaultServerCleanupIntervalSec = 5 * minute
 	DefaultKeepAliveIntervalSec     = 25
 	DefaultMissedKeepAliveThreshold = 3
-	DefaultMaxKernelSendBufferSize  = 2 * 1024 * 1024 // 2 MiB
-	DefaultMaxKernelRecvBufferSize  = 4 * 1024 * 1024 // 4 MiB
+	DefaultMaxKernelSendBufferSize  = 1 * mib
+	DefaultMaxKernelRecvBufferSize  = 1 * mib
 )
 
 var (
