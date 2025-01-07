a42a5130cc744024baaddad007fca1a94ad55143
xeptore <29199390+xeptore@users.noreply.github.com>
2025-01-07 21:42:45 +0330

    lint: fix error
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -171,10 +171,12 @@ func (err *OpenLatestVersionDownloadURLError) Error() string {
 func run(ctx context.Context, logger zerolog.Logger, cfg *config.Client) (err error) {
 	if Version != "dev" {
 		logger.Trace().Str("current_version", Version).Msg("Checking for new releases")
-		if exists, latestTag, err := update.NewerVersionExists(ctx, logger, Version); nil != err {
+		exists, latestTag, err := update.NewerVersionExists(ctx, logger, Version)
+		switch {
+		case nil != err:
 			logger.Error().Err(err).Func(errutil.TreeLog(err)).Msg("Failed to check for newer version existence. Make sure you have internet access and rerun the application.")
 			return nil
-		} else if exists {
+		case exists:
 			logger.Error().Msg("Newer version exists. Download URL will be opened soon")
 			time.Sleep(time.Second)
 			downloadURL := "https://github.com/xeptore/linkos/releases/download/" + latestTag + "/client_" + runtime.GOOS + "_" + runtime.GOARCH + ".zip"
@@ -183,9 +185,9 @@ func run(ctx context.Context, logger zerolog.Logger, cfg *config.Client) (err er
 				return &OpenLatestVersionDownloadURLError{URL: downloadURL, CommandOut: out}
 			}
 			return nil
-		} else if !exists && latestTag != Version {
+		case !exists && latestTag != Version:
 			logger.Warn().Str("current_version", Version).Str("latest_version", latestTag).Msg("You are running a pre-release version.")
-		} else {
+		default:
 			logger.Info().Msg("Already running the latest version")
 		}
 	}
